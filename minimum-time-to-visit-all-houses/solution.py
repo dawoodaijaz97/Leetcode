e93c386d611a177ca3795a53219e83dc72af3390
Dawood <dawood.aijaz97@gmail.com>
2025-07-12 11:50:33 +0000

    feat(solutions): add minimum-time-to-visit-all-houses solution

---
+++ minimum-time-to-visit-all-houses/solution.py
@@ -0,0 +1,10 @@
+def solve(*args, **kwargs):
+    """
+    Placeholder for the actual solution to the problem.
+    This is where you would implement the logic to find the minimum time to visit all houses.
+    
+    :param args: Positional arguments (if any)
+    :param kwargs: Keyword arguments (if any)
+    :return: The result of the computation
+    """
+    pass  # Replace this with actual implementation
