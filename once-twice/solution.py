a917a32faa43a629af9eedf7574fd61d8e204696
Dawood <dawood.aijaz97@gmail.com>
2025-08-06 00:21:33 +0000

    feat(solutions): add once-twice solution

---
+++ once-twice/solution.py
@@ -0,0 +1,12 @@
+def solve(*args, **kwargs):
+    """
+    Placeholder for the actual solution logic.
+
+    Parameters:
+    *args: Variable length argument list.
+    **kwargs: Arbitrary keyword arguments.
+
+    Returns:
+    The result of the computation defined by the problem statement.
+    """
+    pass
