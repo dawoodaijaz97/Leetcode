b6bc22d95dc3a3e2c28b522ed5cc8f8952c0ddcb
Dawood <dawood.aijaz97@gmail.com>
2025-08-10 23:31:19 +0000

    feat(solutions): add process-string-with-special-operations-i solution

---
+++ process-string-with-special-operations-i/solution.py
@@ -0,0 +1,15 @@
+def solve(s: str) -> str:
+    result = []
+    
+    for char in s:
+        if char.islower():
+            result.append(char)
+        elif char == '*':
+            if result:
+                result.pop()
+        elif char == '#':
+            result.extend(result)
+        elif char == '%':
+            result.reverse()
+    
+    return ''.join(result)
